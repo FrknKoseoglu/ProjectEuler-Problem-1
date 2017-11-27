51a3b0c32ec9a60950b6a30488441017e38c7415
FrknKoseoglu <furkan1771@gmail.com>
2017-11-27 13:13:42 +0300

    Project Euler Problem 1 Solution
    Project Euler Problem 1 Solution in C# Console

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main()
+        {
+            int toplam;
+            toplam = 0;
+            for (int i = 0; i < 1000; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                    toplam = toplam + i;
+            System.Console.Write("1000'e kadar 3 yada 5'in katı olan sayıların toplamı: " + toplam);
+            System.Console.WriteLine();
+        }
+    }
+}
